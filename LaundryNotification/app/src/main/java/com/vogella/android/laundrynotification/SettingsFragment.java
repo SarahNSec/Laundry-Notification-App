3a4fa055fc7055b22304cfd24d6bf0c1d72df89d
SarahLN <noles.sarah@gmail.com>
2018-04-01 11:19:59 -0500

    created settings fragment

---
+++ LaundryNotification/app/src/main/java/com/vogella/android/laundrynotification/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.vogella.android.laundrynotification;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load preferences from app_settings.xml file
+        addPreferencesFromResource(R.xml.app_settings);
+    }
+
+}
