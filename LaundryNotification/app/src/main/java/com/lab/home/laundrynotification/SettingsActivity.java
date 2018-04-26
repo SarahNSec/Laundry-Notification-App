989eaf7096a51eace1ee75bbe9b6537a133cc515
SarahLN <noles.sarah@gmail.com>
2018-04-25 21:16:54 -0500

    adding settings activity and fragment

---
+++ LaundryNotification/app/src/main/java/com/lab/home/laundrynotification/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.lab.home.laundrynotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ LaundryNotification/app/src/main/java/com/lab/home/laundrynotification/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.lab.home.laundrynotification;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+}
